754ce3ce4b84d5ffb7f9a82f39b852c3a2ad42fd
hasanulmuttaqin <hasanul.muttaqin@ui.ac.id>
2025-09-09 13:15:25 +0700

    troubleshoot 5

--- main/apps.py
+++ main/apps.py
@@ -1,6 +1,13 @@
 from django.apps import AppConfig
 
-
 class MainConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'main'
+
+    def ready(self):
+        # Run migrations automatically if tables are missing
+        from django.core.management import call_command
+        try:
+            call_command("migrate", interactive=False)
+        except Exception as e:
+            print("Migration failed:", e)
