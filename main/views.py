9e9d9f9b7c643edad35659e363faebd2e2a8c39d
hasanulmuttaqin <hasanul.muttaqin@ui.ac.id>
2025-09-09 13:59:00 +0700

    bisa please....

--- main/views.py
+++ main/views.py
@@ -3,7 +3,6 @@ from .models import Product
 import uuid
 
 def main_view(request):
-    # Try to get the first product, or create a default one
     product, created = Product.objects.get_or_create(
         id=uuid.UUID("00000000-0000-0000-0000-000000000001"),  # fixed UUID so it won't duplicate
         defaults={
