436b951639ef962611aa38c69780140220b2955b
hasanulmuttaqin <hasanul.muttaqin@ui.ac.id>
2025-09-09 13:51:25 +0700

    plis... plis... plis... bisa

--- main/apps.py
+++ main/apps.py
@@ -1,13 +1,6 @@
 from django.apps import AppConfig
 
+
 class MainConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'main'
-
-    def ready(self):
-        # tambah sesuatu di local buat trobleshoot
-        from django.core.management import call_command
-        try:
-            call_command("migrate", interactive=False)
-        except Exception as e:
-            print("Migration failed:", e)
