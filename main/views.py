9ddec3693fb8b6521a6c31f057fbc0be5eda9b1b
hasanulmuttaqin <hasanul.muttaqin@ui.ac.id>
2025-09-09 14:22:56 +0700

    cleaning up

--- main/views.py
+++ main/views.py
@@ -4,13 +4,13 @@ import uuid
 
 def main_view(request):
     product, created = Product.objects.get_or_create(
-        id=uuid.UUID("00000000-0000-0000-0000-000000000001"),  # fixed UUID so it won't duplicate
+        id=uuid.UUID("00000000-0000-0000-0000-000000000001"),
         defaults={
             "name": "Liverpool Original Jersey LFC Away 24/25",
             "brand": "Jersey",
             "price": 899000,
             "description": "Jersey Luar Kandang Liverpool Musim 2024/2025 dengan teknologi DRY-FIT agar tetap nyaman beraktivitas",
-            "thumbnail": "https://example.com/liverpool-jersey.jpg",  # replace with actual image URL
+            "thumbnail": "https://example.com/liverpool-jersey.jpg",
             "category": "Jersey",
             "stock": 25,
             "is_featured": True,
